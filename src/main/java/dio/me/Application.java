8f874428013782fb2b0c900bb56a5aa2c1a6435d
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-10-22 22:55:53 -0300

    change names of Application.java

--- src/main/java/dio/me/Application.java
+++ src/main/java/dio/me/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrcamentoPessoal2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrcamentoPessoal2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/dio/me/ApplicationTests.java
+++ src/test/java/dio/me/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrcamentoPessoal2023ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
