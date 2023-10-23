2cee813cb429cb91e0c8331007cb959aab392a3b
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-10-22 23:38:27 -0300

    model implemented

---
+++ src/main/java/dio/me/model/Despesa.java
@@ -0,0 +1,36 @@
+package dio.me.model;
+
+public class Despesa {
+	private Long id;
+	private Data data;
+	private String descricao;
+	private Double valor;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Data getData() {
+		return data;
+	}
+	public void setData(Data data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+
+
+
+}
---
+++ src/main/java/dio/me/model/Transacao.java
@@ -0,0 +1,50 @@
+package dio.me.model;
+
+public class Transacao {
+private Long id;
+private Data data;
+private String descricao;
+private Double valor;
+private Receita receita;
+private Despesa despesa;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public Data getData() {
+	return data;
+}
+public void setData(Data data) {
+	this.data = data;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public Double getValor() {
+	return valor;
+}
+public void setValor(Double valor) {
+	this.valor = valor;
+}
+public Receita getReceita() {
+	return receita;
+}
+public void setReceita(Receita receita) {
+	this.receita = receita;
+}
+public Despesa getDespesa() {
+	return despesa;
+}
+public void setDespesa(Despesa despesa) {
+	this.despesa = despesa;
+}
+
+
+
+
+}
