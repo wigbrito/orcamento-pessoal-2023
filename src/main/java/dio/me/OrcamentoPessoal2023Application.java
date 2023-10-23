4f7e0f07b2fef952c6fed079a8fa1e80c4c554a4
William Goncalves Brito - PrestServ <wigbrito@gmail.com>
2023-10-22 22:39:27 -0300

    upload of the project spring boot

---
+++ src/main/java/dio/me/OrcamentoPessoal2023Application.java
@@ -0,0 +1,13 @@
+package dio.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoPessoal2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoPessoal2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/OrcamentoPessoal2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoPessoal2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
